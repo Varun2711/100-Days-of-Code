bacab25d7f146afab5a50f978e48196baedf0080
Varun Patel <rvjpatel99@gmail.com>
2024-09-14 13:15:50 -0700

    added comments to day 1 project and setup skeleton for day 2 project

--- day1/BandNameGenerator.py
+++ day1/BandNameGenerator.py
@@ -1,5 +1,15 @@
+# 100 days of code: day 1
+# make a simple band name generator
+
 if __name__ == '__main__':
+    #print out introduction
     print('Welcome to the Band Name Generator')
+
+    # ask for their city
     city = input('What\'s the name of the city you grew up in?\n')
+
+    # ask for their pet's name
     pet = input('What\'s the name of your pet?\n')
+
+    # display result
     print(f'Your band name could be {city} {pet}')
