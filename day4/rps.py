f0a6e70aa439460b377677e8748eba7a95c68bae
Varun Patel <rvjpatel99@gmail.com>
2024-09-14 14:43:24 -0700

    added ascii art to rps.py in day4 directory

---
+++ day4/rps.py
@@ -0,0 +1,9 @@
+# 100 days of code: day 4
+# single play rock paper scissors
+
+import random
+
+# ascii art
+rock = "    _______\n---'   ____)\n      (_____)\n      (_____)\n      (____)\n---.__(___)"
+paper = "    _______\n---'   ____)____\n          ______)\n          _______)\n         _______)\n---.__________)"
+scissors = "    _______\n---'   ____)____\n          ______)\n       __________)\n      (____)\n---.__(___)"
